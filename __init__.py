b3bbc82066c5fcd0f37039d2eb807604a851271b
maweiqiang1978 <maweiqiangster@gmail.com>
2026-03-26 21:15:00 +0800

    实现了节点自动导入
    实现了节点自动导入

--- __init__.py
+++ __init__.py
@@ -1,3 +1,23 @@
-from .AIB import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from pathlib import Path
+import sys
+import os
+import importlib.util
 
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+
+NODE_CLASS_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS = {}
+
+current_dir = Path(__file__).parent
+nodes_folder = current_dir / 'nodes_folder'
+
+for filename in os.listdir(nodes_folder):
+	if filename.startswith('_'):
+		continue
+	module_name = filename[: -3]
+	module_path = os.path.join(nodes_folder, filename)
+	spec = importlib.util.spec_from_file_location(module_name, module_path)
+	module = importlib.util.module_from_spec(spec)
+	spec.loader.exec_module(module)
+	NODE_CLASS_MAPPINGS.update(module.NODE_CLASS_MAPPINGS)
+	NODE_DISPLAY_NAME_MAPPINGS.update(module.NODE_DISPLAY_NAME_MAPPINGS)
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
